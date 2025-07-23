6dbd6591564888af7965576fddf8e6c9647319df
Erfon Spanos <erfonspanos@gmail.com>
2025-07-23 12:38:58 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,22 @@
 package application;
 
+import java.util.Date;
+import java.util.Locale;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
+		
 		Department obj = new Department(1, "Books");
 		
-		System.out.println(obj);
+		Seller seller = new Seller(1, "Mark Revv", "mark@gmail.com", new Date(), 4000.0, obj);
+		
+		System.out.println(seller);
+		
 	}
 }
